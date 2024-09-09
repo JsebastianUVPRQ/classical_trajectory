0ec492ab2d40eb6201319438770a391dff00bbe4
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-09-08 23:20:20 -0500

    1st_attempt_of_a _convolution

--- Semana_9_sep/generador_para_convoluciones.py
+++ Semana_9_sep/generador_para_convoluciones.py
@@ -3,8 +3,8 @@
 # integral de -infinito a infinito de f(t)*g(t- \tau) d(\tau\)
 # En latex:  \left ( f * g \right )_{(t)} \overset{\underset{\mathrm{def}}{}}{\approx} \int_{-\infty }^{\infty} f(\tau) g(t - \tau) dr 
 
-# pseudocodigo:
-
+'''
+pseudocodigo:
 for each image row in input image:
     for each pixel in image row:
         set accumulator to zero
@@ -16,6 +16,7 @@ for each image row in input image:
                 endif
                     
         set output image pixel to accumulator
+''' 
         
 # Ahora traduzcamos este pseudocodigo a python
 
@@ -44,5 +45,9 @@ def convolucion(img, kernel):
     return img
 
 # creemos una imagen de pixeles random para ejemplificar
+imagen_ejemplo = np.random.randint(0, 255, (5, 5))
+kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+imagen_procesada = convolucion(imagen_ejemplo, kernel)
+print(imagen_procesada)
 
-imagen = cv2.imread('images/imagen.jpg', 0)
+# imagen = cv2.imread('images/imagen.jpg', 0)
