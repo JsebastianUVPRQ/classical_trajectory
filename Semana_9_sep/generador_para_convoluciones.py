11290f2211fd9e40cbe51f356575433ada28926c
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-09-08 23:44:30 -0500

    Update generador_para_convoluciones.py

--- Semana_9_sep/generador_para_convoluciones.py
+++ Semana_9_sep/generador_para_convoluciones.py
@@ -1,7 +1,8 @@
-# Formalmente, una convolución se define como una integral 
-# de la forma: 
+# Formalmente, una convolución se define como una integral
+# de la forma:
 # integral de -infinito a infinito de f(t)*g(t- \tau) d(\tau\)
-# En latex:  \left ( f * g \right )_{(t)} \overset{\underset{\mathrm{def}}{}}{\approx} \int_{-\infty }^{\infty} f(\tau) g(t - \tau) dr 
+# En latex:  \left ( f * g \right )_{(t)} \overset{\underset{\mathrm{def}}{}}{\approx}
+# \int_{-\infty }^{\infty} f(\tau) g(t - \tau) dr
 
 '''
 pseudocodigo:
