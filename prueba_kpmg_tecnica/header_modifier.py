06183d0f9b43e444ea251c7398457c4035177777
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-11-08 20:19:18 -0500

    falta dejar listos los nombres

--- prueba_kpmg_tecnica/header_modifier.py
+++ prueba_kpmg_tecnica/header_modifier.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import re
 
 class ExcelCleaner:
     def __init__(self, file_path):
@@ -23,7 +24,7 @@ class ExcelCleaner:
     def clean_column_names(self):
         """Limpia los nombres de las columnas del DataFrame según los requisitos."""
         if self.dataframe is None:
-            raise ValueError("El DataFrame no se ha cargado. Ejecuta 'load_excel' primero.")
+            raise ValueError("El DataFrame no se ha cargado")
         
         # Asignar nombres de columnas genéricos
         self.dataframe.columns = [f'Column{i+1}' for i in range(self.dataframe.shape[1])]
@@ -31,8 +32,22 @@ class ExcelCleaner:
         # Realizar la limpieza de nombres de columnas
         self.dataframe.columns = self.dataframe.columns.str.strip()  # Eliminar espacios al inicio y al final
         self.dataframe.columns = self.dataframe.columns.str.replace(r'\s+', '_', regex=True)  # Reemplazar espacios intermedios con "_"
-        # self.dataframe.columns = self.dataframe.columns.str.replace(r'[^a-zA-Z0-9_]', '', regex=True) Eliminar caracteres especiales
+        self.dataframe.columns = self.dataframe.columns.str.replace(r'[^a-zA-Z0-9_]', '_', regex=True) #Eliminar caracteres especiales
         print("Nombres de columnas modificados correctamente.")
+        
+    def clean_rows(self):
+        """Limpia los valores en todas las filas del DataFrame."""
+        if self.dataframe is None:
+            raise ValueError("El DataFrame no se ha cargado")
+        
+        # Remover espacios en blanco al inicio y al final de cada celda
+        self.dataframe = self.dataframe.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        
+        # Reemplazar espacios intermedios con ""
+        self.dataframe = self.dataframe.applymap(lambda x: re.sub(r'\s+', '_', x) if isinstance(x, str) else x)
+        
+        # Eliminar caracteres especiales y reemplazarlos con "_"
+        self.dataframe = self.dataframe.applymap(lambda x: re.sub(r'[^a-zA-Z0-9_]', '_', x) if isinstance(x, str) else x)
 
     def export_to_csv(self):
         """Exporta el DataFrame a un archivo CSV en el mismo directorio con nombres de columnas corregidos."""
@@ -43,10 +58,10 @@ class ExcelCleaner:
         output_file = os.path.join(self.directory, f"{self.file_name}_corregido.csv")
         
         # Exportar a CSV con punto y coma como delimitador y comillas para los valores
-        self.dataframe.to_csv(output_file, index=False, sep=';', quoting=1)  # quoting=1 usa comillas en todos los valores
-        print(f"Archivo CSV con columnas corregidas guardado en: '{output_file}'")
+        self.dataframe.to_csv(output_file, index=False, sep=';', quoting=1)
+        print("Archivo CSV con columnas corregidas guardado.")
 
-# Ejemplo de uso
+
 if __name__ == "__main__":
     # Ruta del archivo Excel (esta misma carpeta)
     ruta_archivo_excel = "Punto 3_ Datos_muestra_prueba_tecnica.xlsx"
@@ -57,4 +72,5 @@ if __name__ == "__main__":
     # Ejecutar los métodos para cargar, limpiar y exportar
     cleaner.load_excel()
     cleaner.clean_column_names()
+    cleaner.clean_rows()
     cleaner.export_to_csv()
