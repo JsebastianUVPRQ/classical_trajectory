ba5a98908030a083876258d44bcfad3bb2a1240c
JsebastianUVPRQ <penaloza.juan@correounivalle.edu.co>
2024-12-10 22:12:12 -0500

    PIPELINE

--- ptdataknow/entrenamiento_local/test_code_dataknow/preprocessor.py
+++ ptdataknow/entrenamiento_local/test_code_dataknow/preprocessor.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import xgboost as xgb
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-# ------------------------------------------PREPROCESS------------------------------------------
 class DataPreprocessor:
     def __init__(self, lags=5, train_size=0.7):
         self.lags = lags
@@ -31,37 +28,7 @@ class DataPreprocessor:
         X_test_scaled = self.scaler.transform(X_test)
         return X_train_scaled, X_test_scaled
     
-# ------------------------------------------preprocess------------------------------------------
-# ------------------------------------------TRAINER------------------------------------------
-class ModelTrainer:
-    def __init__(self, params=None):
-        if params is None:
-            params = {
-                'objective': 'reg:squarederror',
-                'n_estimators': 3000,
-                'max_depth': 4,
-                'learning_rate': 0.001,
-                'colsample_bytree': 0.3,
-                'reg_alpha': 0.1,
-                'reg_lambda': 0.1
-            }
-        self.params = params
-        self.model = xgb.XGBRegressor(**self.params)
-
-    def train(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-    def evaluate(self, y_true, y_pred):
-        mse = mean_squared_error(y_true, y_pred)
-        return mse
-
-    def get_model(self):
-        return self.model    
-# ------------------------------------------TRAINER------------------------------------------
-# ------------------------------------------PIPELINE------------------------------------------
+    
 class Pipeline:
     def __init__(self, lags=5, train_size=0.7, model_params=None):
         self.preprocessor = DataPreprocessor(lags=lags, train_size=train_size)
